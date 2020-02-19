cbec05ffe27af5039d163fd8038b460b2be7ae8b
krzysztow <krzysztow@gmail.com>
2020-02-19 22:21:01 +0000

    Added simple HTTP request handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
+#include <string>
 #include <iostream>
+#include <pistache/endpoint.h>
 
-int main() {
-    std::cout << "Hello\n";
+using namespace Pistache;
+
+struct HelloHandler:
+       	public Http::Handler 
+{
+  HTTP_PROTOTYPE(HelloHandler)
+
+  void onRequest(const Http::Request&, Http::ResponseWriter writer) override
+  {
+    std::cout << "Handling request\n";
+    writer.send(Http::Code::Ok, "Hello, World!");
+  }
+};
+
+int main() 
+{
+  constexpr char address[] = "*:9080";
+  std::cout << "Starting server at " << address << '\n';
+  Http::listenAndServe<HelloHandler>(Pistache::Address(address));
 }
